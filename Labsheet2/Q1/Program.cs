1ee076a77b79a07240dcc949ff6fa7bf0d6f5fff
Umesh CHILAKA <umesh.chilaka@outlook.com>
2023-09-25 22:13:45 +0530

    labsheet2

---
+++ Labsheet2/Q1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Please input N:");
+                double num = double.Parse(Console.ReadLine());
+
+                if (num - Math.Round(num) == 0)
+                    Console.WriteLine($"{num} is an integer.");
+                else
+                    Console.WriteLine($"{num} is not an integer.");
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
