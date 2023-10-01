04c7bec50252a6c7eb78397923969e8e789d2121
Umesh CHILAKA <umesh.chilaka@outlook.com>
2023-10-01 14:38:27 +0530

    LS6 Q4

--- Labsheet3/Q3/Program.cs
+++ Labsheet3/Q3/Program.cs
@@ -10,42 +10,33 @@ namespace Q3
     {
         static void Main(string[] args)
         {
-            while (true)
+            Console.Write("Enter a shipping type (R-Regular, X-Express): ");
+            var type = Console.ReadKey().KeyChar;
+            var cost = 0.0;
+            if (type == 'R' || type == 'X')
             {
-                /*
-     Enter a shipping type (R-Regular, X-Express): R
-Enter the weight of your book package (kilograms): 4.5
-Your shipping cost is 1375.00 bahts with R-type shipping of 4.5-kilogram book
-
-     */
-                Console.Write("Enter a shipping type (R-Regular, X-Express): ");
-                var type = Console.ReadKey().KeyChar;
-                var cost = 0.0;
-                if (type == 'R' || type == 'X')
+                Console.Write("\r\nEnter the weight of your book package (kilograms): ");
+                var weight = Convert.ToDouble(Console.ReadLine()) * 1000;
+                if (weight <= 2000)
                 {
-                    Console.Write("\r\nEnter the weight of your book package (kilograms): ");
-                    var weight = Convert.ToDouble(Console.ReadLine()) * 1000;
-                    if (weight <= 2000)
-                    {
-                        cost = weight * 0.25;
-                    }
-                    else
-                    {
-                        const double fixedRate = 2000 * 0.25;
-                        cost = fixedRate + (weight - 2000) * 0.35;
-                    }
-                    if (type == 'X')
-                    {
-                        cost += 50;
-                    }
-                    Console.WriteLine($"\r\nYour shipping cost is {cost} bahts with {type}-type shipping of {weight / 1000}-kilogram books");
+                    cost = weight * 0.25;
                 }
                 else
                 {
-                    Console.WriteLine("\r\nYour type is invalid!!");
+                    const double fixedRate = 2000 * 0.25;
+                    cost = fixedRate + (weight - 2000) * 0.35;
                 }
-                Console.WriteLine("\r\n"); 
+                if (type == 'X')
+                {
+                    cost += 50;
+                }
+                Console.WriteLine($"\r\nYour shipping cost is {cost} bahts with {type}-type shipping of {weight/1000}-kilogram books");
             }
+            else
+            {
+                Console.WriteLine("\r\nYour type is invalid!!");
+            }
+            Console.WriteLine("\r\n");
         }
     }
 }
