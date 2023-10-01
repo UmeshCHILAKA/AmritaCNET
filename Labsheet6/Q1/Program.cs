cacdc36641ae4f29bbdf18e8430c319541940eb0
Umesh CHILAKA <umesh.chilaka@outlook.com>
2023-10-01 17:12:51 +0530

    Labsheet 6 Q1

---
+++ Labsheet6/Q1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder cyl = new Cylinder();
+            Console.WriteLine($"Default Cylinder class initiated");
+            Console.WriteLine($"Volume of Cylinder is {cyl.getVolume()}");
+
+            cyl = new Cylinder(10);
+            Console.WriteLine($"Cylinder with radius={cyl.getRadius()} initiated");
+            Console.WriteLine($"Volume of Cylinder is {cyl.getVolume()}");
+
+            cyl = new Cylinder(10, 1);
+            Console.WriteLine($"Cylinder with radius={cyl.getRadius()} and height={cyl.getVolume()} initiated");
+            Console.WriteLine($"Volume of Cylinder is {cyl.getVolume()}");
+
+            cyl = new Cylinder(10, 0.1, "Green");
+            Console.WriteLine($"Cylinder with radius={cyl.getRadius()} and height={cyl.getVolume()} of {cyl.getColor()} color initiated");
+            Console.WriteLine($"Volume of Cylinder is {cyl.getVolume()}");
+
+            Console.ReadKey();
+        }
+    }
+}
