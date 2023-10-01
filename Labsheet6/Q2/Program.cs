4e8ca471c247dc3cb17cce3ee86ea09ebdbc697b
Umesh CHILAKA <umesh.chilaka@outlook.com>
2023-10-01 17:46:42 +0530

    Labsheet 6 Q2

---
+++ Labsheet6/Q2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Circle();
+
+            Console.WriteLine($"Default instance of Circle");
+            Console.WriteLine($"Circle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+
+            shape = new Circle(5);
+            Console.WriteLine($"Circle instance with radius of 5");
+            Console.WriteLine($"Circle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+
+            shape = new Circle(10, "green", false);
+            Console.WriteLine($"Circle instance with radius of 10 with color green and not filled");
+            Console.WriteLine($"Circle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+
+            shape = new Circle(1, "Yellow", true);
+            Console.WriteLine($"Circle instance with radius of 1 with color Yellow and filled");
+            Console.WriteLine($"Circle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+
+            Console.WriteLine("----------------------Rectangle----------------------");
+
+            shape = new Rectangle();
+
+            Console.WriteLine($"Default instance of Rectangle");
+            Console.WriteLine($"Rectangle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+
+            shape = new Rectangle(2, 3);
+            Console.WriteLine($"Rectangle instance with width of 2 and lenght of 3");
+            Console.WriteLine($"Rectangle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+
+            shape = new Rectangle(1, 5, "green", false);
+            Console.WriteLine($"Rectangle instance with with width of 1 and lenght of 5, with color green and not filled");
+            Console.WriteLine($"Rectangle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+
+            shape = new Rectangle(4, 6, "Yellow", true);
+            Console.WriteLine($"Rectangle instance with with width of 4 and lenght of 6, with color Yellow and filled");
+            Console.WriteLine($"Rectangle details = {shape},\r\narea = {shape.getArea()} and perimeter = {shape.getPerimeter()}");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Labsheet6/Q2/Shape.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal abstract class Shape
+    {
+        protected string _color;
+        protected bool _filled;
+        protected Shape() : this("Red", true)
+        { }
+        protected Shape(string color, bool filled)
+        {
+            _color = color;
+            _filled = filled;
+        }
+
+        /// <summary>
+        /// Gets the color.
+        /// </summary>
+        public string getColor()
+        {
+            return _color;
+        }
+
+        /// <summary>
+        /// Determines whether this instance is filled.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this shape is filled; otherwise, <c>false</c>.
+        /// </returns>
+        public bool isFilled()
+        {
+            return _filled;
+        }
+
+        /// <summary>
+        /// Sets the color.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        public void setColor(string color)
+        {
+            _color = color;
+        }
+
+        /// <summary>
+        /// Sets the filled.
+        /// </summary>
+        /// <param name="filled">if set to <c>true</c> [filled].</param>
+        public void setFilled(bool filled)
+        {
+            _filled = filled;
+        }
+
+        /// <summary>
+        /// Gets the area.
+        /// </summary>
+        public abstract double getArea();
+
+        /// <summary>
+        /// Gets the perimeter.
+        /// </summary>
+        public abstract double getPerimeter();
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"Shape [color = {_color}, filled = {_filled}]";
+        }
+    }
+}
