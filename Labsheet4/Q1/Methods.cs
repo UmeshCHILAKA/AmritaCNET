5fac3cc6ae207c0c92c20a9b137615068e07474c
Umesh CHILAKA <umesh.chilaka@outlook.com>
2023-10-02 12:47:05 +0530

    LS-4 final

---
+++ Labsheet4/Q1/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace lab7
+{
+    class ex4
+    {
+        static int max(int a, int b)
+        {
+           return  Math.Max(a, b);
+        }
+        static double avg(int a, int b)
+        {
+           return (a + b)/2;
+        }
+        static int mul(int a, int b)
+        {
+            return a*b;
+        }
+        static int celling(double a)
+        {
+            return Convert.ToInt32( Math.Ceiling(a));
+        }
+        static void Main()
+        {
+            int i = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            Console.WriteLine(" max = {0} ", max(i, j));
+            Console.WriteLine(" avg = {0} ", avg(i, j));
+            Console.WriteLine(" mul = {0} ", mul(i, j));
+            Console.WriteLine(" celling = {0} ", celling(i));
+        }
+    }
+}
